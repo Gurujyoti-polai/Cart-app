f08d338b2f0ea1659d0f76beb594827ab8211f02
Gurujyoti polai <1905881@kiit.ac.in>
2021-11-23 11:06:24 +0530

    8. Deleting products from Cart

--- src/CartItem.js
+++ src/CartItem.js
@@ -4,6 +4,12 @@ class CartItem extends React.Component {
     render() {
         console.log('this.props', this.props);
         const { price, title, qty } = this.props.product;
+        const {
+            product,
+            onIncreaseQuantity,
+            onDecreaseQuantity,
+            onDeleteProduct
+          } = this.props;
         return (
             <div className="cart-item">
                 <div className="left-block">
@@ -17,14 +23,15 @@ class CartItem extends React.Component {
                         {/*Buttons*/}
                         <img
                          alt="increase" className="action-icons" src="https://cdn-icons.flaticon.com/png/512/3303/premium/3303893.png?token=exp=1637384375~hmac=f93642a42c38b04921f62a15a94abfe0" 
-                         onClick={() => this.props.onIncreaseQuantity(this.props.product)}
+                         onClick={() => onIncreaseQuantity(product)}
                         />
                         <img
                          alt="decrease" className="action-icons" src="https://as2.ftcdn.net/v2/jpg/04/60/56/57/500_F_460565747_PE6uYbtfeXASxkZpibOoN6SKUWTUHPvY.jpg" 
-                         onClick={this.decreaseQuantity}
+                         onClick={() => onDecreaseQuantity(product)}
                         />
                         <img
-                         alt="delete" className="action-icons" src="https://cdn-icons.flaticon.com/png/512/484/premium/484611.png?token=exp=1637384550~hmac=e4cda09401617836b40f8606dd842bde" 
+                         alt="delete" className="action-icons" src="https://cdn-icons.flaticon.com/png/512/484/premium/484611.png?token=exp=1637384550~hmac=e4cda09401617836b40f8606dd842bde"
+                         onClick={() => onDeleteProduct(product.id)} 
                         />
                     </div>
                 </div> 
